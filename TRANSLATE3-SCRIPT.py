c603cbd90ac4423adfdfd7d2ae335df049f4e051
efe <cemreefe@gmail.com>
2020-04-23 16:13:04 +0300

    improve seo (html lang, h1 titles, meta description), homogenize languages

--- TRANSLATE3-SCRIPT.py
+++ TRANSLATE3-SCRIPT.py
@@ -44,7 +44,7 @@
 # 
 # 
 
-# In[55]:
+# In[73]:
 
 
 #for colab
@@ -54,7 +54,7 @@
 ## Character limit: 15K
 
 
-# In[56]:
+# In[74]:
 
 
 #import os
@@ -62,7 +62,7 @@
 #os.system(cmd)
 
 
-# In[57]:
+# In[75]:
 
 
 from googletrans import Translator
@@ -71,7 +71,7 @@ import re
 translator = Translator()
 
 
-# In[58]:
+# In[76]:
 
 
 def find_between( s, first, last ):
@@ -83,14 +83,14 @@ def find_between( s, first, last ):
         return ""
 
 
-# In[59]:
+# In[77]:
 
 
 def string_strip(s):
     return re.sub(r"[^A-Za-z0-9]", "-", s)
 
 
-# In[60]:
+# In[78]:
 
 
 from urllib.request import Request, urlopen
@@ -173,7 +173,7 @@ def sciencedaily_parse_article(url):
   
 
 
-# In[61]:
+# In[79]:
 
 
 def get_translation(article_dictionary,target_language):
@@ -215,7 +215,7 @@ def get_translation(article_dictionary,target_language):
     return translations
 
 
-# In[62]:
+# In[80]:
 
 
 ("# deprecated")
@@ -228,7 +228,7 @@ def get_language_dictionary(target_language):
     return ld
 
 
-# In[63]:
+# In[81]:
 
 
 def html_from_dictionary(translated_dictionary, target_language, language_dictionary): # translated & language could be just one dictionary this was stupid.
@@ -239,16 +239,19 @@ def html_from_dictionary(translated_dictionary, target_language, language_dictio
   
     html = open(SUBFOLDER + "jp.temp").read()
 
-    html_article = re.sub(r"\n\n", "</p>\n\n<p>",   translated_dictionary["article"])
-    html_article = "<p>" + html_article + "</p>"
+    article_text = re.sub(r"\n\n", "</p>\n\n<p>",   translated_dictionary["article"])
+    html_article = "<p>" + article_text + "</p>"
 
     print(translated_dictionary["headline"])
   
     html = re.sub(r"\$\$article-title%%",  translated_dictionary["headline"],         html)
     html = re.sub(r"\$\$img-alt%%",        translated_dictionary["imgalt"],           html)
     html = re.sub(r"\$\$article-meta%%",   translated_dictionary["meta"],             html)
-    html = re.sub(r"\$\$source%%",         translated_dictionary["citation"],            html)
-    html = re.sub(r"\$\$img.jpg%%",        translated_dictionary["imgaddr"],             html)
+    
+    html = re.sub(r"\$\$meta-tag-text%%",  article_text[:150],                        html)
+    
+    html = re.sub(r"\$\$source%%",         translated_dictionary["citation"],         html)
+    html = re.sub(r"\$\$img.jpg%%",        translated_dictionary["imgaddr"],          html)
     html = re.sub(r"\$\$article-text%%",   html_article                  ,            html)
   
     html = re.sub(r"\$\$home%%",           language_dictionary["homepage-"+target_language],           html)
@@ -256,7 +259,7 @@ def html_from_dictionary(translated_dictionary, target_language, language_dictio
     html = re.sub(r"\$\$about%%",          language_dictionary["about us-"+target_language],           html)
     html = re.sub(r"\$\$jp-translation%%", language_dictionary["journal pacifique-"+target_language],  html)
     html = re.sub(r"\$\$taken-from%%",     language_dictionary["source:-"+target_language],            html)
-    html = re.sub(r"\$\$target-languages%%",     target_language,            html)
+    html = re.sub(r"\$\$target-language%%",     target_language,            html)
   
     html = re.sub(r"\$\$imgcredit%%",       translated_dictionary["imgcredit"],          html)
   
@@ -272,7 +275,7 @@ def html_from_dictionary(translated_dictionary, target_language, language_dictio
   
 
 
-# In[64]:
+# In[82]:
 
 
 def getnewpath(translated_dict):
@@ -303,13 +306,13 @@ def getnewpath(translated_dict):
     return [newheadline,newpathaddr,urlheadline]
 
 
-# In[65]:
+# In[83]:
 
 
 SUBFOLDER = ""
 
 
-# In[66]:
+# In[84]:
 
 
 import pickle
@@ -323,7 +326,7 @@ def load_obj(name ):
         return pickle.load(f)
 
 
-# In[67]:
+# In[85]:
 
 
 def dic_to_dirfile(dic,target_language, elements_dictionary):
@@ -348,7 +351,7 @@ def dic_to_dirfile(dic,target_language, elements_dictionary):
     return [htmlx,tr_di,newdirs[2],newdirs[0]]
 
 
-# In[68]:
+# In[86]:
 
 
 def targdic_to_dirfile(tr_di,target_language, elements_dictionary):
@@ -369,59 +372,86 @@ def targdic_to_dirfile(tr_di,target_language, elements_dictionary):
     return [htmlx,tr_di,newdirs[2],newdirs[0]]
 
 
-# In[129]:
+# In[87]:
 
 
 elements_dictionary = {
     "journal pacifique-es":"periódico pacífico", 
-    "homepage-es":"página principal", 
-    "archive-es":"archivo",
-    "about us-es":"sobre nosotros",
-    "source:-es":"fuente:",
     "journal pacifique-pt":"jornal pacífico", 
-    "homepage-pt":"pagina inicial", 
-    "archive-pt":"arquivo",
-    "about us-pt":"sobre nós",
-    "source:-pt":"fonte:",
     "journal pacifique-tr":"barışçıl gazete", 
-    "homepage-tr":"anasayfa", 
-    "archive-tr":"arşiv",
-    "about us-tr":"hakkında",
-    "source:-tr":"kaynakça:",
     "journal pacifique-zh-CN":"和平的报纸", 
-    "homepage-zh-CN":"主页", 
-    "archive-zh-CN":"档案",
-    "about us-zh-CN":"关于我们",
-    "source:-zh-CN":"资源",
     "journal pacifique-pl":"spokojna gazeta", 
+    "journal pacifique-de":"friedliche Zeitung", 
+    "journal pacifique-af":"vreedsame koerant",
+    
+    "homepage-es":"página principal", 
+    "homepage-pt":"pagina inicial", 
+    "homepage-tr":"anasayfa",
     "homepage-pl":"strona główna", 
+    "homepage-de":"Startseite", 
+    "homepage-zh-CN":"主页", 
+    "homepage-af":"tuisblad", 
+    
+    "archive-es":"archivo",
+    "archive-pt":"arquivo",
+    "archive-tr":"arşiv",
+    "archive-zh-CN":"档案",
     "archive-pl":"archiwum",
+    "archive-de":"Archiv",
+    "archive-af":"argief",
+    
+    
+    "about us-es":"sobre nosotros",
+    "about us-pt":"sobre nós",
+    "about us-tr":"hakkında",
+    "about us-zh-CN":"关于我们",
     "about us-pl":"o nas",
+    "about us-de":"Über uns",
+    "about us-af":"oor ons",
+    
+    "source:-es":"fuente:",
+    "source:-pt":"fonte:",
+    "source:-tr":"kaynakça:",
+    "source:-zh-CN":"资源",
     "source:-pl":"źródło",
+    "source:-de":"Quelle:",
+    "source:-af":"bron:",
+    
     "latest-articles-es":"Últimos artículos",
     "latest-articles-pt":"Artigos Mais Recentes",
     "latest-articles-pl":"Ostatnie artykuły",
     "latest-articles-zh-CN":"最新的文章",
-    "latest_articles-tr":"En yeni makaleler",
+    "latest-articles-tr":"En yeni makaleler",
+    "latest-articles-de":"Neueste Artikel",
+    "latest-articles-af":"Mees onlangse artikels",
+    
     "hometext-es":"Bienvenido a Journal Pacifique. Le proporcionamos los últimos artículos sobre ciencia y tecnología. Journal Pacifique se dedica a la distribución de investigaciones científicas populares en otros idiomas además del inglés.",
     "hometext-pt":"Bem-vindo ao Journal Pacifique. Fornecemos os artigos mais recentes sobre ciência e tecnologia. O Journal Pacifique é dedicado à distribuição de pesquisas científicas populares em outros idiomas que não o inglês.",
     "hometext-pl":"Witamy w Journal Pacifique. Zapewniamy najnowsze artykuły na temat nauki i technologii. Czasopismo Pacifique poświęcone jest rozpowszechnianiu popularnych badań naukowych w językach innych niż angielski.",
     "hometext-zh-CN":"欢迎来到Journal Pacifique。 我们为您提供有关科学和技术的最新文章。 Journal Pacifique致力于以英语以外的语言分发流行的科学研究。",
     "hometext-tr":"Journal Pacifique'e hoş geldiniz. Bilim ve teknoloji ile ilgili en son makaleleri size sunuyoruz. Journal Pacifique, popüler bilimsel araştırmaların İngilizce dışındaki dillerde dağıtımına adanmıştır.",
+    "hometext-de":"Willkommen im Journal Pacifique. Wir präsentieren Ihnen die neuesten Artikel über Wissenschaft und Technologie. Das Journal Pacifique widmet sich der Verbreitung populärwissenschaftlicher Forschung in anderen Sprachen als Englisch.",
+    "hometext-af":"Welkom by die Journal Pacifique. Ons bied die nuutste artikels oor wetenskap en tegnologie aan. Die Journal Pacifique is toegewy aan die verspreiding van populêre wetenskaplike navorsing in ander tale as Engels.",
+    
     "abouttext-es":"Journal Pacifique se estableció para evitar que la barrera del idioma obstruya la accesibilidad de los artículos científicos. En Journal Pacifique pensamos que todos los artículos científicos deberían ser fácilmente accesibles para las personas en su propio idioma. Por lo tanto, hemos asumido la responsabilidad de encontrar artículos en Internet de valor decente y traducirlos a tantos idiomas como sea posible, haciéndolos accesibles a muchas más personas de lo que era posible en su idioma original.",
     "abouttext-pt":"O Journal Pacifique foi criado para impedir que a barreira do idioma obstrua a acessibilidade de artigos científicos. No Journal Pacifique, pensamos que todos os artigos científicos devem ser facilmente acessíveis às pessoas em seu próprio idioma. Portanto, assumimos a responsabilidade de encontrar artigos na Internet de valor decente e traduzi-los para o maior número possível de idiomas, tornando-os acessíveis a muito mais pessoas do que era possível em seu idioma original.",
     "abouttext-pl":"Journal Pacifique został utworzony, aby bariera językowa nie utrudniała dostępu do artykułów naukowych. W Journal Pacifique uważamy, że wszystkie artykuły naukowe powinny być łatwo dostępne dla ludzi w ich własnym języku. Dlatego podjęliśmy się odpowiedzialności za znalezienie artykułów w Internecie o przyzwoitej wartości i przetłumaczenie ich na jak najwięcej języków, dzięki czemu będą dostępne dla większej liczby osób niż było to możliwe w ich oryginalnym języku.",
     "abouttext-zh-CN":"Journal Pacifique的建立是为了防止语言障碍阻碍科学文章的可访问性。 Journal Pacifique认为我们所有的科学文章都应该易于人们以自己的语言阅读。 因此，我们承担了在互联网上查找具有体面价值的文章并将其翻译成尽可能多的语言的责任，从而使更多的人可以使用原始语言。",
     "abouttext-tr":"",
+    "abouttext-de":"Das Journal Pacifique wurde gegründet, um zu verhindern, dass die Sprachbarriere die Zugänglichkeit wissenschaftlicher Artikel behindert. Wir bei Journal Pacifique glauben, dass alle wissenschaftlichen Artikel für Menschen in ihrer eigenen Sprache leicht zugänglich sein sollten. Aus diesem Grund haben wir die Verantwortung dafür übernommen, Artikel mit angemessenem Wert im Internet zu finden und in so viele Sprachen wie möglich zu übersetzen, um sie für viel mehr Menschen zugänglich zu machen, als dies in ihrer Originalsprache möglich war.",
+    "abouttext-af":"Tydskrif Pacifique is gestig om te voorkom dat die taalversperring die toeganklikheid van wetenskaplike artikels belemmer. In Journal Pacifique glo ons dat alle wetenskaplike artikels maklik toeganklik moet wees vir mense in hul eie taal. Daarom het ons die verantwoordelikheid aanvaar om artikels met 'n ordentlike waarde op die internet te vind en dit in soveel tale as moontlik te vertaal, sodat dit vir baie meer mense toeganklik is as wat in hul oorspronklike taal moontlik was.",
+    
     "see-more-es":"Ver más",
     "see-more-pt":"Ver mais",
     "see-more-pl":"Zobacz więcej",
     "see-more-zh-CN":"看更多",
     "see-more-tr":"Daha fazla",
+    "see-more-de":"Mehr sehen",
+    "see-more-af":"Sien meer"
 }
 
 
-# In[130]:
+# In[88]:
 
 
 def url_to_dirfile(url,target_language):
@@ -433,7 +463,7 @@ def url_to_dirfile(url,target_language):
     return [htmlx,ar_di]
 
 
-# In[131]:
+# In[89]:
 
 
 def get_article_urls_sd():
@@ -455,21 +485,22 @@ def get_article_urls_sd():
     return links
 
 
-# In[132]:
+# In[90]:
 
 
 articles = get_article_urls_sd()
             
-languages = ["es","pt","pl","zh-CN"]
+languages = ["es", "pt", "pl", "zh-CN", "de", "af"]
+#languages = ["de", "af"]
 
 
-# In[133]:
+# In[91]:
 
 
 articles
 
 
-# In[134]:
+# In[92]:
 
 
 #headlinessofar = []
@@ -477,7 +508,7 @@ articles
 #articleurlssofar = []
 
 
-# In[135]:
+# In[93]:
 
 
 headlinessofar = load_obj("headlinessofar")
@@ -485,7 +516,19 @@ articlessofar = load_obj("articlessofar")
 articleurlssofar =load_obj("articleurlssofar")
 
 
-# In[136]:
+# In[94]:
+
+
+articlessofar
+
+
+# In[ ]:
+
+
+
+
+
+# In[95]:
 
 
 #refresh already saved articles' html
@@ -496,7 +539,7 @@ for article in articlessofar:
     tmp = targdic_to_dirfile(articlessofar[article], articlessofar[article]["lang"], elements_dictionary)
 
 
-# In[137]:
+# In[96]:
 
 
 import time
@@ -506,12 +549,16 @@ for article in articles:
     if (article not in articleurlssofar) or test_enable:
         dic = sciencedaily_parse_article(article)
         for target_language in languages:
+            try:
                 tmp = dic_to_dirfile(dic,target_language, elements_dictionary)
                 articlessofar[tmp[2]]=tmp[1]
+            except KeyError:
+                url_to_dirfile(article, target_language)
         articleurlssofar.append(article)
+        
 
 
-# In[138]:
+# In[104]:
 
 
 save_obj(headlinessofar,     "headlinessofar")
@@ -519,7 +566,7 @@ save_obj(articlessofar,       "articlessofar")
 save_obj(articleurlssofar, "articleurlssofar")
 
 
-# In[139]:
+# In[98]:
 
 
 #get most recent 9 articles and get their "keys"
@@ -589,7 +636,7 @@ def refresh_indices():
         f.write(htmlx)
 
 
-# In[140]:
+# In[99]:
 
 
 #get most recent 9 articles and get their "keys"
@@ -656,7 +703,7 @@ def refresh_archives():
         f.write(htmlx)
 
 
-# In[141]:
+# In[100]:
 
 
 #get most recent 9 articles and get their "keys"
@@ -700,19 +747,19 @@ def refresh_abouts():
 
 
 
-# In[146]:
+# In[101]:
 
 
 refresh_archives()
 
 
-# In[143]:
+# In[102]:
 
 
 refresh_abouts()
 
 
-# In[147]:
+# In[103]:
 
 
 refresh_indices()
